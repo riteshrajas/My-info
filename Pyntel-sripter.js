537fb13d872f68aa7fd101bcbace2cd62c2e5cce
RITESH RAJ AS <riteshrajas@users.noreply.github.com>
2023-12-25 15:55:12 -0500

    Added Cookies

--- Pyntel-sripter.js
+++ Pyntel-sripter.js
@@ -1,15 +1,27 @@
 
-window.addEventListener('load', function() {
+
+window.addEventListener('load', function () {
 	const splashScreen = document.getElementById('splash-screen');
 	const content = document.getElementById('content');
-	setTimeout(function() { splashScreen.style.display = 'none'; content.style.display = 'block'; }, 2000);
+	setTimeout(function () { splashScreen.style.display = 'none'; content.style.display = 'block'; }, 2000);
+	var cachedUsername = Cookies.get('username');
+	var cachedPassword = Cookies.get('password');
+
+	if (cachedUsername && cachedPassword) {
+		const usernameInput = document.getElementById('Username');
+		usernameInput.value = cachedUsername;
+		const passwordInput = document.getElementById('Password');
+		passwordInput.value = cachedPassword;
+
+	}
 });
 
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
-   
+
+
 
 function Connect_Clicked() {
 	var submit = document.getElementById("connect");
@@ -19,52 +31,29 @@ function Connect_Clicked() {
 
 	if (username == "201" && password == "password") { alert("Welcome admin"); }
 
-	fetch("https://script.google.com/macros/s/AKfycbxmnwpzT62OLCfuR9w3I282udbZN4QvPy9J-I6Dyk99wsTitzHL69dHV44fkH1kHcXu3g/exec?id="+username+"&pass="+password+"&connect=true")
+	fetch("https://script.google.com/macros/s/AKfycbxcd9n_6AjNtbCaOe1RE7dB6OBngy68wXKUzFJxRKrLbzd__tyKqXD1Izqj_QIx_lPmew/exec?id="+username+"&pass="+password+"&connect=true")
 		.then(response => response.json())
 		.then(data => {
 			const datasheet = document.getElementById("ruleover");
 			const content = document.getElementById('content');
 			const bridge = document.getElementById('content-ruleover-merger');
+
+
 			content.style.display = 'none';
 			datasheet.style.display = 'none';
 			bridge.style.display = 'block';
-			// console.log(data);
+			console.log(data);
 
 			if (data['Correct'] == "true") {
-				var Sucess_or_Fail = document.getElementById('Sucess/fail_tablet');
-				Sucess_or_Fail.innerText = "Sucessful!";
-				sleep(10000).then(() => {
-					fetch("https://script.google.com/macros/s/AKfycbxmnwpzT62OLCfuR9w3I282udbZN4QvPy9J-I6Dyk99wsTitzHL69dHV44fkH1kHcXu3g/exec?id="+username+"&pass="+password+"&connect=false")
-					.then(response => response.json())
-					.then(data => {
-						// console.log(data);
-						content.style.display = 'none';
-						datasheet.style.display = 'block';
-						bridge.style.display = 'none';
+				Cookies.set('username', username);
+				Cookies.set('password', password);
+				addGame();
 
-						var Teamer = document.getElementById('Teamrole');
-						var Signed_up_points = document.getElementById('signed_up_points');
-						var General_requirements = document.getElementById('General_Requirements');
-						var Attendance = document.getElementById('AttendanceProgress');
-						var Funds = document.getElementById('Fund_raised');
-						var Text_attendance = document.getElementById('Attendance_text');
-						var Text_General_requirements = document.getElementById('General_Requirements_text');
-						var Text_Funds = document.getElementById('textfunds');
+				GetLatestInfo(username, password)
 
-						Teamer.innerText = data['TeamRole'] + '!';
-						Signed_up_points.innerText = ('Signed Up Points : ' + data['Projects_signed_up'] + ' Points');
-						General_requirements.setAttribute("value", data['General_Requirements(%)']);
-						Attendance.setAttribute("value", data['Attendance(%)']);
-						Funds.setAttribute("value", data['Fundrasising(%)']);
-						Text_General_requirements.innerText = ('General Requirements: ' + data['General_Requirements(%)'] + "%");
-						Text_attendance.innerText = ('Attendance: ' + data['Attendance(%)'] + "%");
-						Text_Funds.innerText = ('Fundraising: ' + data['Fundrasising(%)'] + "%");
-
-
-					})
-				  });
-				
 			}
+
+
 			if (data['Correct'] == "false") {
 				var Sucess_or_Fail = document.getElementById('Sucess/fail_tablet');
 				var condition = document.getElementById('condense');
@@ -73,7 +62,7 @@ function Connect_Clicked() {
 				// close()
 			}
 
-			
+
 		})
 
 		.catch(error => {
@@ -82,19 +71,94 @@ function Connect_Clicked() {
 		});
 
 }
-function Register_Connected(){
+function Register_Connected() {
 	var First_Name = document.getElementById('Register-First-name').value;
 	var Last_Name = document.getElementById('Register-Last-name').value;
 	var Username = document.getElementById('Register-Username').value;
 	var Password = document.getElementById('Register-Password').value;
-	fetch("https://script.google.com/macros/s/AKfycbxRayPdqflxciAaqQzxeEQbOYvrHe912tD3RebKRmwZWI69-4CK0zhXGXoddD41kPLp/exec?firstname="+First_Name+"&lastname="+Last_Name+"&schoolID="+Username+"&password="+ Password)
-	.then(response => response.json())
+	fetch("https://script.google.com/macros/s/AKfycbxRayPdqflxciAaqQzxeEQbOYvrHe912tD3RebKRmwZWI69-4CK0zhXGXoddD41kPLp/exec?firstname=" + First_Name + "&lastname=" + Last_Name + "&schoolID=" + Username + "&password=" + Password)
+		.then(response => response.json())
 	var button = document.getElementById("register");
 	button.innerText = "Registering Profile... ( > 5 seconds)";
 	sleep(1000).then(() => {
 		button.innerText = "Thanks, You can close the Tab now!";
 		sleep(1000).then(() => {
-			location.href='index.html'
+			location.href = 'index.html'
 		})
 	})
 }
+
+
+function GetLatestInfo(username, password) {
+	sleep(1000).then(() => {
+	fetch("https://script.google.com/macros/s/AKfycbxcd9n_6AjNtbCaOe1RE7dB6OBngy68wXKUzFJxRKrLbzd__tyKqXD1Izqj_QIx_lPmew/exec?id=" + username + "&pass=" + password + "&connect=false")
+		.then(response => response.json())
+		.then(data => {
+			// console.log(data);
+			const datasheet = document.getElementById("ruleover");
+			const content = document.getElementById('content');
+			const bridge = document.getElementById('content-ruleover-merger');
+
+
+			content.style.display = 'none';
+			datasheet.style.display = 'block';
+			bridge.style.display = 'none';
+
+			var Teamer = document.getElementById('Teamrole');
+			var Signed_up_points = document.getElementById('signed_up_points');
+			var General_requirements = document.getElementById('General_Requirements');
+			var Attendance = document.getElementById('AttendanceProgress');
+			var Funds = document.getElementById('Fund_raised');
+			var Text_attendance = document.getElementById('Attendance_text');
+			var Text_General_requirements = document.getElementById('General_Requirements_text');
+			var Text_Funds = document.getElementById('textfunds');
+
+			Teamer.innerText = data['TEAM_ROLE'] + '!';
+			Signed_up_points.innerText = ('Signed Up Points : ' + data['SIGNED_UP_POINTS'] + ' Points');
+			General_requirements.setAttribute("value", data['GENERAL_REQUIREMENTS_PERCENTAGE']);
+			Attendance.setAttribute("value", data['ATTENDANCE_PERCENTAGE']);
+			Funds.setAttribute("value", data['FUNDRAISING_PERCENTAGE']);
+			Text_General_requirements.innerText = ('General Requirements: ' + data['GENERAL_REQUIREMENTS_PERCENTAGE'] + "%");
+			Text_attendance.innerText = ('Attendance: ' + data['ATTENDANCE_PERCENTAGE'] + "%");
+			Text_Funds.innerText = ('Fundraising: ' + data['FUNDRAISING_PERCENTAGE'] + "%");
+
+
+			for (const key in data) {
+				if (data.hasOwnProperty(key)) {
+					Cookies.set(key, data[key]);
+				}
+			}
+		})
+	})
+}
+
+function addGame() {
+	const datasheet = document.getElementById("ruleover");
+	const content = document.getElementById('content');
+	const bridge = document.getElementById('content-ruleover-merger');
+  
+	content.style.display = 'none';
+	datasheet.style.display = 'block';
+	bridge.style.display = 'none';
+  
+	// Retrieve values from cookies
+	var Teamer = document.getElementById('Teamrole');
+	var Signed_up_points = document.getElementById('signed_up_points');
+	var General_requirements = document.getElementById('General_Requirements');
+	var Attendance = document.getElementById('AttendanceProgress');
+	var Funds = document.getElementById('Fund_raised');
+	var Text_attendance = document.getElementById('Attendance_text');
+	var Text_General_requirements = document.getElementById('General_Requirements_text');
+	var Text_Funds = document.getElementById('textfunds');
+  
+	Teamer.innerText = Cookies.get('TEAM_ROLE') + '!' + " Fetching Latest Data";
+	Signed_up_points.innerText = ('Signed Up Points : ' + Cookies.get('SIGNED_UP_POINTS') + ' Points');
+	General_requirements.setAttribute("value", Cookies.get('GENERAL_REQUIREMENTS_PERCENTAGE'));
+	Attendance.setAttribute("value", Cookies.get('ATTENDANCE_PERCENTAGE'));
+	Funds.setAttribute("value", Cookies.get('FUNDRAISING_PERCENTAGE'));
+	Text_General_requirements.innerText = ('General Requirements: ' + Cookies.get('GENERAL_REQUIREMENTS_PERCENTAGE') + "%");
+	Text_attendance.innerText = ('Attendance: ' + Cookies.get('ATTENDANCE_PERCENTAGE') + "%");
+	Text_Funds.innerText = ('Fundraising: ' + Cookies.get('FUNDRAISING_PERCENTAGE') + "%");
+	console.log(Cookies.get('Teamrole'));
+  }
+  
